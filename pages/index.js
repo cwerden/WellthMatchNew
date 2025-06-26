cb879f103c57fac23eeeff79def368d5c78ae65a
cwerden <cwerden@ceteraadvisors.com>
2025-06-26 14:01:31 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,9 @@
-<REPLACE_WITH_UPDATED_QUIZ_CODE>
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", padding: "4rem" }}>
+      <h1>Welcome to WellthMatch</h1>
+      <p>This is the starting page for your financial advisor lead gen app.</p>
+      <p>More features coming soon.</p>
+    </div>
+  );
+}
